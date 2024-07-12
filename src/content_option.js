67bedac92eac8b4f90bcc208e0475c8826603a81
rahulmalu1998 <rahulmalu1998@gmail.com>
2024-07-13 02:03:07 +0530

    First edit

---
+++ src/content_option.js
@@ -0,0 +1,192 @@
+const logotext = "JOHN";
+const meta = {
+  title: "Rahul Malu",
+  description:
+    "A full stack engineer, currently working in Bangalore, an IITKGP alumnus",
+};
+
+const introdata = {
+  title: "I’m Rahul",
+  animated: {
+    first: "I love to code and debug",
+    second: "Down to take new challenges",
+    third: "Backend + frontend development using JS",
+  },
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+  your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+  title: "",
+  aboutme:
+    "Having finished my graduation from IIT Kharagpur in 2022, I have been wokring at Enphase and it has been nothing but an enriching experience, getting the hang of corporate work culture and how the whole software development cycle works. Am now looking to broaden my scope of skills and diversify my knowledge base. Looking for a similar yet different role in software where I feel my skills can be put to use.",
+};
+const worktimeline = [
+  {
+    jobtitle: "Full stack web developer",
+    where: "Enphase Energy",
+    date: "2022",
+  },
+  {
+    jobtitle: "Software Engineer",
+    where: "Bloutout",
+    date: "2021-2022",
+  },
+  {
+    jobtitle: "Devops Intern",
+    where: "Citymall",
+    date: "2021",
+  },
+];
+
+const skills = [
+  {
+    name: "NodeJS",
+    value: 90,
+  },
+  {
+    name: "Python",
+    value: 65,
+  },
+  {
+    name: "JS/TS",
+    value: 80,
+  },
+  {
+    name: "React",
+    value: 70,
+  },
+  {
+    name: "C++",
+    value: 60,
+  },
+  {
+    name: "Sql",
+    value: 70,
+  },
+];
+
+const services = [
+  {
+    title: "Front end development",
+    description:
+      "2+yrs of experience developing front end application from scratch and developing UI using libraries like MUI and React framework",
+  },
+  {
+    title: "API development",
+    description:
+      "2+ yrs of experience developing APIs in NodeJS express framework and writing unit tests in Mocha",
+  },
+  {
+    title: "CI/CD automation",
+    description:
+      "Used docker and kubernetes to deploy applications in production, and monitor the servers using monitoring tools like Grafana and Prometheus",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/550/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "rahulmalu1998@gmail.com",
+  YOUR_FONE: "+91-7980587146",
+  description:
+    "Available to discuss work opportunities and common interests on all modes of communications mentioned",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com/rahulmalu1998",
+  instagram: "https://www.instagram.com/rahul.malu1/",
+  linkedin: "https://www.linkedin.com/in/rahul-malu-a97a50169/",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
